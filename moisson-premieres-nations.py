5119625f6ec85c6766732975acee3f9c351ef6a4
Jean-Philippe Guilbault <jp.guilbault@protonmail.com>
2017-03-14 10:22:03 -0400

    Update moisson-premieres-nations.py

--- moisson-premieres-nations.py
+++ moisson-premieres-nations.py
@@ -7,6 +7,7 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
+#Toutes les possibilités de numéros de réserve (je doute qu'il y en ait plus de 1000).
 reserves = list (range(0,1000))
 
 entetes = {
@@ -14,12 +15,14 @@ entetes = {
 	"From":"jp.guilbault@protonmail.com"
 }
 
+#Je teste toutes les combinaisons d'url possibles pour les pages «Main» des réserves autochtones pour déterminer celles qui fonctionnent.
 fonctionne = "url-aandc.txt"
 url = open(fonctionne,"w")
 
 for reserve in reserves:
     url1 = "http://fnp-ppn.aandc-aadnc.gc.ca/fnp/Main/Search/FNMain.aspx?BAND_NUMBER={}&lang=eng".format(reserve)
     reponses = requests.get(url1, headers=entetes)
+#Si elles fonctionnent, je les entrepose dans un fichier .txt à réutiliser pour plus tard.
     if reponses.status_code == 200:
         url.write(url1)
         url.write("\n")
@@ -46,11 +49,13 @@ lignes = f.readlines()
 
 url2 = []
 
+#Je reprends mes urls qui fonctionnent pour en extraire le numéro de réserve (je dois faire cette étape car les pages /FNRegPopulation.aspx?BAND_NUMBER=... me renvoient une page fonctionnelle même si le numéro de réserve n'est associé à aucune réserve réelle.
 for ligne in lignes:
     numeros = re.findall('\d+', ligne)
     numero = numeros[0]
     url2.append("http://fnp-ppn.aandc-aadnc.gc.ca/fnp/Main/Search/FNRegPopulation.aspx?BAND_NUMBER={}&lang=fra".format(numero))
 
+#Dans ces nouvelles urls fonctionnelles je vais faire mon moissonnage de statistiques de population (merci pour le coup de pouce lors du dernier cours!)
 for url in url2:
     stats = []
     demandes = requests.get(url,headers=entetes)
@@ -58,7 +63,8 @@ for url in url2:
     stats.append(pages.find("span", id="plcMain_txtBandName").text)
     for item in pages.find("div", class_="table-responsive").find_all("tr")[1:]:
         stats.append(item.find_all("span")[1].text)
-    
+
+#Je transcris mes résultats dans un fichier .csv
     ecrire = open(fichier,"a")
     pouet = csv.writer(ecrire)
     pouet.writerow(stats)
